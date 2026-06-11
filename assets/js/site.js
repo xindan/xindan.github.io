f51f833d5f43a73c2d8c67bafefeaa60e5496c0b
Rui Zhang <rui@puffin.local>
2026-06-11 16:11:57 -0400

    JS

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,20 +2,21 @@
   const hamburger = document.getElementById('hamburger');
   const navigation = document.getElementById('navigation');
   const year = document.getElementById('year');
-  let timer;
+  let hideTimer;
+  let resizeTimer;
 
   year.textContent = new Date().getFullYear();
 
   hamburger.onclick = function() {
     hamburger.classList.toggle('active');
-    clearTimeout(timer);
+    clearTimeout(hideTimer);
     if (!navigation.classList.contains('show')) {
       navigation.classList.remove('hide');
       navigation.classList.add('show');
     } else {
       navigation.classList.remove('show');
       navigation.classList.add('hide');
-      timer = setTimeout(function() {
+      hideTimer = setTimeout(function() {
         navigation.classList.remove('hide');
       }, 300);
     }
@@ -23,11 +24,14 @@
   }
 
   window.addEventListener('resize', function () {
-    if (window.innerWidth > 1023) {
-      hamburger.classList.remove('active');
-      navigation.classList.remove('show', 'hide');
-      document.body.classList.remove('no-scroll');
-    }
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function () {
+      if (window.innerWidth > 1023) {
+        hamburger.classList.remove('active');
+        navigation.classList.remove('show', 'hide');
+        document.body.classList.remove('no-scroll');
+      }
+    }, 150);
   });
 
   window.addEventListener('scroll', function () {
